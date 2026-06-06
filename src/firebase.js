1c876a30722be635383f846a084cf6a16c4bc4f8
Evgeny Dubrovin <evgen@192.168.100.2>
2026-06-06 14:41:27 +0700

    Step 3: move Firebase config to .env

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD22Sx8OLYEz0YUWjI6xNksmnQHtQbPNRI",
-  authDomain: "voice-scheduler-988c4.firebaseapp.com",
-  projectId: "voice-scheduler-988c4",
-  storageBucket: "voice-scheduler-988c4.firebasestorage.app",
-  messagingSenderId: "616021115542",
-  appId: "1:616021115542:web:4db46c56e1c36a32ea38b7",
-  measurementId: "G-6WRT3MGCH1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
