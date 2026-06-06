45d6fac64712f9cd2334b600645472db85ea9dde
Evgeny Dubrovin <evgen@192.168.100.2>
2026-06-06 14:17:23 +0700

    Step 3: clean App layout + Login manages auth state

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,13 @@ import Login from "./components/Login";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="text-blue-700">Voice Scheduler</h1>
-        <h2>Hi there!</h2>
-      </div>
-
+    <div>
+      <h1 className="text-blue-700">Voice Scheduler</h1>
       <div className="mt-12">
         <Login />
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
